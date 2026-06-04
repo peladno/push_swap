ad3195e100e5f60a7c5763503115d2c6bcd58729
peladno <j.perezurrutia@gmail.com>
2026-06-04 21:13:28 +0900

    feature(operations/reverse_rotate): main reverse_rotate plus rra, rrb, rrr done

--- src/javi/operations/reverse_rotate.c
+++ src/javi/operations/reverse_rotate.c
@@ -6,8 +6,42 @@
 /*   By: jperez-u <jperez-u@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 20:36:34 by jperez-u          #+#    #+#             */
-/*   Updated: 2026/06/02 20:37:34 by jperez-u         ###   ########.fr       */
+/*   Updated: 2026/06/04 21:11:13 by jperez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static t_operation	reverse_rotate(t_stack *stack)
+{
+	t_node	*old_bottom;
+
+	if (!stack || stack->size < 2)
+		return (OP_NOT_DONE);
+	old_bottom = stack->bottom;
+	stack->bottom = old_bottom->prev;
+	stack->bottom->next = NULL;
+	old_bottom->prev = NULL;
+	old_bottom->next = stack->top;
+	stack->top->prev = old_bottom;
+	stack->top = old_bottom;
+	return (OP_DONE);
+}
+
+void	rra(t_stack *a)
+{
+	if (reverse_rotate(a) == OP_DONE)
+		ft_putstr("rra\n");
+}
+
+void	rrb(t_stack *b)
+{
+	if (reverse_rotate(b) == OP_DONE)
+		ft_putstr("rrb\n");
+}
+
+void	rrr(t_stack *a, t_stack *b)
+{
+	if (reverse_rotate(a) == OP_DONE && reverse_rotate(b) == OP_DONE)
+		ft_putstr("rrr\n");
+}
