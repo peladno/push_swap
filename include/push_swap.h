3e1a2b40440593513d32cf2441da44af7be06ac0
peladno <j.perezurrutia@gmail.com>
2026-06-11 21:05:37 +0900

    delete: delete some files and move files to algorithms directory

--- include/push_swap.h
+++ include/push_swap.h
@@ -57,58 +57,6 @@ typedef enum e_operation
 	OP_DONE
 }					t_operation;
 
-<<<<<<< HEAD
-typedef struct s_bench
-{
-	long			total_ops;
-
-	long			sa;
-	long			sb;
-	long			ss;
-	long			pa;
-	long			pb;
-	long			ra;
-	long			rb;
-	long			rr;
-	long			rra;
-	long			rrb;
-	long			rrr;
-
-	char			*strategy;
-	int				n;
-	long			rotations;
-	long			inserts;
-	long			max_depth;
-
-}					t_bench;
-
-int					parse_args(int argc, char **argv, t_stack *a,
-						t_strategy *strategy);
-void				free_split(char **tokens);
-int					coord_compress(t_stack *stack);
-t_status			validate_token(const char *token, int *out_value);
-int					has_duplicates(t_stack *stack_a);
-
-t_node				*create_node(int data);
-void				stack_init(t_stack *stack);
-void				stack_free(t_stack *stack_a);
-t_status			stack_push_bottom(t_stack *stack_a, int value);
-
-void				sa(t_stack *a);
-void				sb(t_stack *b);
-void				ss(t_stack *a, t_stack *b);
-
-void				pa(t_stack *a, t_stack *b);
-void				pb(t_stack *a, t_stack *b);
-
-void				rra(t_stack *a);
-void				rrb(t_stack *b);
-void				rrr(t_stack *a, t_stack *b);
-
-void				ra(t_stack *a);
-void				rb(t_stack *b);
-void				rr(t_stack *a, t_stack *b);
-=======
 t_node				*create_node(int value);
 void				stack_init(t_stack *stack_a);
 void				stack_free(t_stack *stack_a);
@@ -131,7 +79,6 @@ t_operation			rra(t_stack *stack_a);
 t_operation			rrb(t_stack *stack_b);
 t_operation			rrr(t_stack *stack_a, t_stack *stack_b);
 void				radix_sort(t_stack *stack_a, t_stack *stack_b);
->>>>>>> master
 
 t_node				*find_x_below(t_stack *s, int x);
 void				rotate_b_to_top(t_stack *b, t_node *target);
